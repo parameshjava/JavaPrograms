da4138ec0160b383d1f77e00153c3f5ec893e669
Paramesh <paramesh.java5@gmail.com>
2016-05-23 03:24:31 +0530

    Corrected IO test cases

--- src/test/java/com/java/io/FileHandlingByStreamTest.java
+++ src/test/java/com/java/io/FileHandlingByStreamTest.java
@@ -1,5 +1,9 @@
 package com.java.io;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,20 +13,21 @@ import com.java.io.FileHandlingByByteStream;
 import junit.framework.TestCase;
 
 public class FileHandlingByStreamTest extends TestCase {
-    
+
     FileHandlingByByteStream stream;
-    
+
     @Before
     public void setUp() {
         stream = new FileHandlingByByteStream();
     }
-    
+
     @Test
     public void testReadFile() {
-        String filePath = "C:\\PARAMESH\\LOGS\\Files\\ByteStreamFile.txt";
+        String filePath = new File("").getAbsolutePath().concat(File.separator)
+                .concat(String.join(File.separator, "src", "test", "resources", "ByteStreamFile.txt"));
         String str = stream.readFile(filePath);
         String[] strings = str.split("\n");
-        
+
         Assert.assertEquals("This is my first string sending to file by using byte oriented stream.", strings[0]);
         Assert.assertEquals("This is my second string sending to file by using byte oriented stream.", strings[1]);
     }
